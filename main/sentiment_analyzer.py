2dcb1f0425a74f5f361a532ecbe1ea84aa9b0590
Full name <email address>
2023-04-26 14:39:41 +0100

    Moving to google colab notebooks to resolve issues

---
+++ main/sentiment_analyzer.py
@@ -0,0 +1,19 @@
+#Using Bert to detect sentiment in text
+
+#First we will install tensorflow and do a rough sentiment analysis via IMDb movie reviews to enure that the 
+#installation and operation is correct.
+
+import os
+import shutil
+
+import tensorflow as tf
+import tensorflow_hub as hub
+import tensorflow_text as text
+
+##Need to resolve tf-models issue. For now, using google notebooks
+
+
+
+
+
+
