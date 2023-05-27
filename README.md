5cf5f8bfe68e5d7fdc513a9019891f28f0ca181c
AnujGore <anujgore23@gmail.com>
2023-05-27 23:11:34 +0100

    Environment created; uploading requirements.txt

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # CHARLES
 Using an RNN to detect stock variability
+
+This project is a multi component analysis of stock variability. I will use sentiment from news and other sources as a metric for the variability, as well as some others like fourier transforms.
+
+## Step 1:
+
+Using tensorflow to detect sentiment from tweets.
+
--- main/sentiment_analyzer.py
+++ main/sentiment_analyzer.py
@@ -1,16 +1,34 @@
 #Using Bert to detect sentiment in text
 
-#First we will install tensorflow and do a rough sentiment analysis via IMDb movie reviews to enure that the 
-#installation and operation is correct.
+import numpy as np
 
-import os
-import shutil
-
+import tensorflow_datasets as tfds
 import tensorflow as tf
-import tensorflow_hub as hub
-import tensorflow_text as text
 
-##Need to resolve tf-models issue. For now, using google notebooks
+tfds.disable_progress_bar()
+
+dataset, info = tfds.load('sentiment140', with_info=True, as_supervised=True)
+
+train_dataset, test_dataset = dataset['train'], dataset['test']
+
+train_dataset.element_spec
+
+for example, label in train_dataset.take(1):
+  print('text: ', example.numpy())
+  print('label: ', label.numpy())
+
+
+BUFFER_SIZE = 10000
+BATCH_SIZE = 64
+
+train_dataset = train_dataset.shuffle(BUFFER_SIZE).batch(BATCH_SIZE).prefetch(tf.data.AUTOTUNE)
+test_dataset = test_dataset.batch(BATCH_SIZE).prefetch(tf.data.AUTOTUNE)
+
+VOCAB_SIZE = 1000
+encoder = tf.keras.layers.TextVectorization(max_tokens=VOCAB_SIZE)
+encoder.adapt(train_dataset.map(lambda text, label: text))
+
+vocab = np.array(encoder.get_vocabulary())
 
 
 
